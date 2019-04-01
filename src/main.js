64195ed73b93117a43bada92f14b52fb1d47798a
Jeremy Eissler <jeissler0@gmail.com>
2019-04-01 09:28:01 -0500

    adds cli ssr plugin changes

--- src/main.js
+++ src/main.js
@@ -1,14 +1,42 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createRouter } from './router'
+import { createStore } from './store'
 import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
-new Vue({
+export async function createApp ({
+          beforeApp = () => {},
+          afterApp = () => {}
+        } = {}) {
+          const router = createRouter()
+          const store = createStore()
+          const apolloProvider = createProvider({
+            ssr: process.server,
+          })
+
+          await beforeApp({
+            router,
+            store,
+            apolloProvider,
+          })
+
+          const app = new Vue({
   router,
   store,
-  apolloProvider: createProvider(),
+  apolloProvider,
   render: h => h(App)
-}).$mount('#app')
+})
+
+          const result = {
+            app,
+            router,
+            store,
+            apolloProvider,
+          }
+
+          await afterApp(result)
+
+          return result
+        }

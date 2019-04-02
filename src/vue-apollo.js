6586e5a42774c09127c5990d17e78891440604fb
Jeremy Eissler <jeissler0@gmail.com>
2019-04-02 10:30:53 -0500

    set fetch policy

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -70,7 +70,7 @@ export function createProvider (options = {}) {
     defaultClient: apolloClient,
     defaultOptions: {
       $query: {
-        fetchPolicy: 'network-only',
+        fetchPolicy: 'cache-and-network',
       },
     },
     errorHandler (error) {
